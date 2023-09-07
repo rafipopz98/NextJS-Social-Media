ae9d5641c7f6ab451355708a70abff63bf9c9123
rafipopz98 <itsrealrafi@gmail.com>
2023-09-07 06:56:09 +0530

    login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,61 @@
+"use client";
 import Icons from "@/components/Icons/Icons";
 import ButtonsText from "@/components/buttonsText/ButtonsText";
-import {FaGoogle,FaGithub} from 'react-icons/fa'
+import { FaGoogle, FaGithub } from "react-icons/fa";
+const googleB = "linear-gradient(to right, #919192 0%, #eff2f9 79%);";
 const HomePage = () => {
-  const googleB="linear-gradient(to right, #919192 0%, #eff2f9 79%);"
+  const initialData = {
+    email: "",
+    password: "",
+  };
+  let name, value;
+  const [formData, setFormData] = initialData;
+
+  const inputHandler = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const submitHandler = () => {
+    console.log("submitted");
+  };
+
   return (
     <div className="container">
       <div className="loginContainer">
         <h2>WELCOME</h2>
         <div className="inputContainer">
-          <input type="text" placeholder="email" />
-          <input type="text" placeholder="password" />
+          <input
+            name="email"
+            value={initialData.email}
+            type="text"
+            placeholder="email"
+            onChange={inputHandler}
+          />
+          <input
+            name="password"
+            value={initialData.password}
+            type="password"
+            placeholder="password"
+            onChange={inputHandler}
+          />
         </div>
         <ButtonsText>Login</ButtonsText>
         <h5 className="loginWith">or Login With</h5>
         <hr className="horizontalLine" />
         <div className="iconsContainer">
-        <Icons color={googleB}>
-        <FaGoogle />
-        </Icons>
-        <Icons color={googleB}>
-        <FaGithub />
-        </Icons>
+          <Icons color={googleB}>
+            <FaGoogle />
+          </Icons>
+          <Icons color={googleB}>
+            <FaGithub />
+          </Icons>
         </div>
-          <h4 className="forgotPassword">Forgot password?</h4>
+        <h4 className="forgotPassword">Forgot password?</h4>
       </div>
     </div>
   );

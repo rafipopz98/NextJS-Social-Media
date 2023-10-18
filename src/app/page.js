c64ebf4c3a0dcdc035e568b5974a4ee080af5731
rafipopz98 <itsrealrafi@gmail.com>
2023-10-18 22:17:14 +0530

    some commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,14 @@
 'use client'
 import Auth from "@/components/Auth/page";
 import Feed from "@/components/Feed/page";  
-import ForgotPassword from "@/components/ForotPassword/page";
-import Signin from "@/components/Login/page";
 import React, { useState } from "react";
 
 const HomePage = () => {
 
-  // const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
   
 
-  return (
-    <Signin />
-  )
-
+  return isAuthenticated?<Feed/>:<Auth/>;
   
 };
 
